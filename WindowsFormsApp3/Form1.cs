bb5183b8df687928fdcc90a1e22ad67c0e570d65
Artem-orloff <80464436+Artem-orloff@users.noreply.github.com>
2022-06-22 18:09:07 +0600

    Add files via upload

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Addition.Calculate(a, b);
+            textBox3.Text = Convert.ToString(c);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Substraction.Calculate(a, b);
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Multiplication.Calculate(a, b);
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Division.Calculate(a, b);
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Degree.Calculate(a, b);
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Remains.Calculate(a, b);
+            textBox3.Text = Convert.ToString(c);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/Remains.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    class Remains
+    {
+        public static double Calculate(double a, double b)
+        {
+            return a % b;
+        }
+    }
+}
